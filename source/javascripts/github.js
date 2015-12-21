2104bb89c18373518ecbe75608e66fd7d1b4b1cf
Chris Henry <henry.christopher@gmail.com>
2015-12-21 17:39:01 -0500

    Open Source contributions page

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -11,11 +11,49 @@ var github = (function(){
     t.innerHTML = fragment;
   }
 
+  function renderContributions(target, contributions){
+    var i = 0, fragment = '', t = $(target)[0];
+
+    for(repo in contributions) {
+
+      if(repo.match('chrishenry') || repo.match('geonosis')) {
+        continue;
+      }
+
+      repo_url = getRepoUrlFromPull(contributions[repo][0].pull_request.html_url)
+      repo_name = getRepoNameFromPull(contributions[repo][0].pull_request.html_url)
+
+      // Append a header
+      fragment += '<h2><a href="'+repo_url+'">'+repo_name+'</a></h4>'
+      fragment += '<ul>'
+      for(i = 0; i < contributions[repo].length; i++) {
+        console.log(contributions[repo][i])
+
+        pr = contributions[repo][i]
+        fragment += '<li><a href="'+pr.pull_request.html_url+'">'+pr.title+'</a></li>';
+      }
+      fragment += '</ul>'
+
+    }
+
+    t.innerHTML = fragment;
+  }
+
+  function getRepoUrlFromPull(pull_url){
+     return pull_url.split('pull')[0]
+  }
+
+  function getRepoNameFromPull(pull_url){
+     repo_url = getRepoUrlFromPull(pull_url)
+     repo_name = repo_url.replace('https://github.com/', '')
+     return repo_name.substr(0, repo_name.length - 1)
+  }
+
   return {
     showRepos: function(options){
       $.ajax({
           url: "https://api.github.com/users/"+options.user+"/repos"
-        , error: function (err) { $(options.target + ' p.loading').addClass('error').text("Error loading feed"); }
+        , error: function (err) { $(options.target + ' p.loading').addClass('error').text("Error loading repos"); }
         , success: function(data) {
           var repos = [];
           for (var i = 0; i < data.length; i++) {
@@ -32,6 +70,33 @@ var github = (function(){
           if (options.count) { repos.splice(options.count); }
           renderRepos(options.target, repos);
         }
+      });
+    },
+    showContributions: function(options){
+      $.ajax({
+        url: "https://api.github.com/search/issues"
+       ,data: {
+          q: 'author:chrishenry is:pr is:public',
+          ref: 'searchresults',
+          type: 'Issues',
+          utf: 'utf8=%E2%9C%93'
+        }
+       ,error: function (err) { $(options.target + ' p.loading').addClass('error').text("Error loading Contributions"); }
+       ,success: function(data) {
+         var repos = {}
+
+         // Group by repo
+         for(var i = 0; i < data.items.length; i++) {
+           repo = getRepoUrlFromPull(data.items[i].html_url)
+           if(typeof(repos[repo]) !== 'undefined') {
+             repos[repo].push(data.items[i])
+           } else {
+             repos[repo] = []
+             repos[repo].push(data.items[i])
+           }
+         }
+         renderContributions(options.target, repos)
+       }
       });
     }
   }
--- source/open-source/index.markdown
+++ source/open-source/index.markdown
@@ -1,6 +1,6 @@
 ---
 layout: page
-title: "OSS Projects and Contributions"
+title: "Open Source Contributions"
 comments: true
 sharing: true
 footer: true
@@ -8,17 +8,9 @@ footer: true
 
 <script src="{{ root_url }}/javascripts/github.js"></script>
 <script type="text/javascript">
-  github.showRepos({user:'chrishenry', target: '#repo-target'})
   github.showContributions({user:'chrishenry', target: '#contrib-target'})
 </script>
 
-
-<h3>Projects</h3>
-<div id="repo-target">
-  <p>Loading...</p>
-</div>
-
-<h3>Contributions</h3>
 <div id="contrib-target">
   <p>Loading...</p>
 </div>
